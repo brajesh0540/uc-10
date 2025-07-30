42bcad07ff1338c1531c92628b839af141b0843a
brajesh0540 <brajesh540@gmail.com>
2025-07-30 22:49:34 +0530

    needs:terraform

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,14 +1,6 @@
-output "alb_dns_name" {
-  description = "The DNS name of the ALB"
-  value       = aws_lb.ecs_alb.dns_name
+output "repositories" {
+  value = {
+    for repo in var.ecr_repo_name : repo => aws_ecr_repository.ecr[repo].repository_url
+  }
   
-}
-
-output "appointments_target_group_arn" {
-  description = "The ARN of the ALBb"
-  value       = aws_lb_target_group.appointments.arn
-}
-
-output "patients_target_group_arn" {
-  value = aws_lb_target_group.patients.arn
 }
