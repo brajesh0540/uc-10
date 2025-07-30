e4165f28fdd4fc6e3ec039639c886a104554ef47
brajesh0540 <brajesh540@gmail.com>
2025-07-30 21:16:46 +0530

    uc-10

--- backend.tf
+++ backend.tf
@@ -1,60 +1,9 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  
+terraform {
+    backend "s3" {
+        bucket         = "my-terraform-state-bucket-brajesh"
+        key            = "dev/terraform.tfstate"
+        encrypt        = true
+        region = "us-east-1"
+      
+    }
 }
-
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-  
-}
-
-variable "public_subnets" {
-  description = "List of public subnets for the VPC"
-  type        = list(string)
-  
-}
-
-variable "private_subnets" {
-  description = "List of private subnets for the VPC"
-  type        = list(string)
-  
-}
-
-variable "alb_sg_name" {
-  description = "Security group ID for the ALB"
-  type        = string
-  
-}
-
-variable "ecs_sg_name" {
-  description = "Security group ID for the ECS service"
-  type        = string
-  
-}
-
-variable "ecr_repo_name" {
-  description = "The desired number of instances for the ECS services"
-  type        = list(string)
-  
-}
-
-variable "execution_role_arn" {
-  description = "ARN of the execution role for ECS tasks"
-  type        = string
-  
-}
-
-variable "azs" {
-  description = "List of availability zones"
-  type        = list(string)
-  
-}
-
-variable "cluster_name" {
-  description = "Name of the ECS cluster"
-  type        = string
-}
-
-
