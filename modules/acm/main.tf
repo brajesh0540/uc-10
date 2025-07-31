7d7e0879d8012fdedbdceedbe7a1760114be3750
brajesh0540 <brajesh540@gmail.com>
2025-08-01 01:51:47 +0530

    state delete

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -39,4 +39,7 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
   depends_on = [aws_route53_record.cert_validation]
+  timeouts {
+    create = "60m"
+  }
 }
