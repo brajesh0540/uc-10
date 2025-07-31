f8b8cfc2e1953d6967e5299fbd4b78f12879adff
brajesh0540 <brajesh540@gmail.com>
2025-08-01 01:29:08 +0530

    needs:terraform

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -1,16 +1,7 @@
-
-
-
 resource "aws_route53_zone" "primary" {
   name = var.domain_name
   # other options if any
 }
-
-
-
-
-
-
 resource "aws_route53_record" "alb_dns" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = var.domain_name  # e.g., "auth.example.com"
